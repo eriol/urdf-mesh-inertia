9abc817cb238c0d32cfa5772b09becf3336e9d80
Daniele Tricoli <eriol@mornie.org>
2024-05-07 19:48:26 +0200

    Ignore E501

--- src/urdf_mesh_inertia/xml.py
+++ src/urdf_mesh_inertia/xml.py
@@ -1,9 +1,10 @@
 # Copyright © 2024 Daniele Tricoli <eriol@mornie.org>
 # SPDX-License-Identifier: BSD-3-Clause
 
+
 INERTIAL_TEMPLATE = """<inertial>
   <origin xyz="{{p.center_of_mass[0]}} {{p.center_of_mass[1]}} {{p.center_of_mass[2]}}"/>
   <mass value="{{p.mass}}"/>
   <inertia ixx="{{p.inertia_tensor[0][0]}}" ixy="{{p.inertia_tensor[0][1]}}" ixz="{{p.inertia_tensor[0][2]}}" iyy="{{p.inertia_tensor[1][1]}}" iyz="{{p.inertia_tensor[1][2]}}" izz="{{p.inertia_tensor[2][2]}}"/>
 </inertial>
-"""
+"""  # noqa: E501
