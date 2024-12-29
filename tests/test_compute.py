0dcc677547d52fa0bd55aa00d1a20d6fbb65de75
Daniele Tricoli <eriol@mornie.org>
2024-12-29 16:14:07 +0100

    Use pytest.approx

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -1,6 +1,7 @@
 # Copyright © 2024 Daniele Tricoli <eriol@mornie.org>
 # SPDX-License-Identifier: BSD-3-Clause
 
+from pytest import approx
 
 from urdf_mesh_inertia.mesh import compute_inertial_parameters
 
@@ -13,21 +14,21 @@ def test_inertia_sphere(sphere_mesh_path):
 
     inertial_parameters = compute_inertial_parameters(sphere_mesh_path, 1)
 
-    assert inertial_parameters.round(6).center_of_mass[0] == 0
-    assert inertial_parameters.round(6).center_of_mass[1] == 0
-    assert inertial_parameters.round(6).center_of_mass[2] == 0
+    assert inertial_parameters.round(6).center_of_mass[0] == approx(0)
+    assert inertial_parameters.round(6).center_of_mass[1] == approx(0)
+    assert inertial_parameters.round(6).center_of_mass[2] == approx(0)
 
-    assert inertial_parameters.round(6).inertia_tensor[0][0] == 9.998421
-    assert inertial_parameters.round(6).inertia_tensor[0][1] == 0
-    assert inertial_parameters.round(6).inertia_tensor[0][2] == 0
+    assert inertial_parameters.round(6).inertia_tensor[0][0] == approx(9.998421)
+    assert inertial_parameters.round(6).inertia_tensor[0][1] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[0][2] == approx(0)
 
-    assert inertial_parameters.round(6).inertia_tensor[1][0] == 0
-    assert inertial_parameters.round(6).inertia_tensor[1][1] == 9.998421
-    assert inertial_parameters.round(6).inertia_tensor[1][2] == 0
+    assert inertial_parameters.round(6).inertia_tensor[1][0] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[1][1] == approx(9.998421)
+    assert inertial_parameters.round(6).inertia_tensor[1][2] == approx(0)
 
-    assert inertial_parameters.round(6).inertia_tensor[2][0] == 0
-    assert inertial_parameters.round(6).inertia_tensor[2][1] == 0
-    assert inertial_parameters.round(6).inertia_tensor[2][2] == 9.997895
+    assert inertial_parameters.round(6).inertia_tensor[2][0] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[2][1] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[2][2] == approx(9.997895)
 
 
 def test_inertia_ellipsoid(ellipsoid_mesh_path):
@@ -38,21 +39,21 @@ def test_inertia_ellipsoid(ellipsoid_mesh_path):
 
     inertial_parameters = compute_inertial_parameters(ellipsoid_mesh_path, 1)
 
-    assert inertial_parameters.round(6).center_of_mass[0] == 0
-    assert inertial_parameters.round(6).center_of_mass[1] == 0
-    assert inertial_parameters.round(6).center_of_mass[2] == 0
+    assert inertial_parameters.round(6).center_of_mass[0] == approx(0)
+    assert inertial_parameters.round(6).center_of_mass[1] == approx(0)
+    assert inertial_parameters.round(6).center_of_mass[2] == approx(0)
 
     # I_x = 1/5 * m * (b**2 + c**2) = 65
-    assert inertial_parameters.round(6).inertia_tensor[0][0] == 65.001317
-    assert inertial_parameters.round(6).inertia_tensor[0][1] == 0
-    assert inertial_parameters.round(6).inertia_tensor[0][2] == 0
+    assert inertial_parameters.round(6).inertia_tensor[0][0] == approx(65.001317)
+    assert inertial_parameters.round(6).inertia_tensor[0][1] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[0][2] == approx(0)
 
     # I_y = 1/5 * m * (a**2 + c**2) = 50
-    assert inertial_parameters.round(6).inertia_tensor[1][0] == 0
-    assert inertial_parameters.round(6).inertia_tensor[1][1] == 50.001317
-    assert inertial_parameters.round(6).inertia_tensor[1][2] == 0
+    assert inertial_parameters.round(6).inertia_tensor[1][0] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[1][1] == approx(50.001317)
+    assert inertial_parameters.round(6).inertia_tensor[1][2] == approx(0)
 
     # I_y = 1/5 * m * (a**2 + b**2) = 25
-    assert inertial_parameters.round(6).inertia_tensor[2][0] == 0
-    assert inertial_parameters.round(6).inertia_tensor[2][1] == 0
-    assert inertial_parameters.round(6).inertia_tensor[2][2] == 24.997895
+    assert inertial_parameters.round(6).inertia_tensor[2][0] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[2][1] == approx(0)
+    assert inertial_parameters.round(6).inertia_tensor[2][2] == approx(24.997895)
